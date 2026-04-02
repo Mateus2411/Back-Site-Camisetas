b70e7f935def342f166a4e4b60ef46344ff515e3
Mateus2411 <mateushenriquedasilva2411@gmail.com>
2026-04-01 21:40:19 -0300

    fix: resolve sqlite3 GLIBC error, use env PORT, ignore database.sqlite

--- config/db.js
+++ config/db.js
@@ -4,7 +4,6 @@ const path = require("path");
 const db = new sqlite3.Database(path.join(__dirname, "..", "database.sqlite"));
 
 db.serialize(() => {
-  db.run(`DROP TABLE IF EXISTS camisetas_selecionadas`);
   db.run(`
     CREATE TABLE camisetas_selecionadas (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
